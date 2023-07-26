1e0615e6cf1a933adff8b774ce4f7dd51db59690
zokirjonmurotov <zokirmurotov863@gmail.com>
2023-07-26 14:12:38 +0500

    js(1,2,3)lessons added

---
+++ js3-dars(let,const,var)/main.js
@@ -0,0 +1,21 @@
+// let, const, var
+
+/*
+let - ES6da qo'shilgan 'variable declaration' hisoblanadi. 
+biz 'let' dan odatda yaratilgandan keyin qiymati yana o'zgaradigan o'zgaruvchilarni yaratishda ishlatamiz!✅
+*/
+
+/*
+const - bu ham 'let' kabi ES6da qo'shilgan variable declaration hisoblanadi.
+biz 'const' dan yaratilganidan keyin qiymati o'zgarmaydigan o'zgaruvchilarni yaratishda ishlatamiz!✅
+*/
+
+/*
+var - bu JavaScriptning ilk versiyada ham mavjud bo'lgan.
+odatda 'var' ni 'let' ga o'xshatishadi lekin ularning bir biridan anchagina farqlari bor misol uchun
+🟢scoping 
+🟢hoisting
+  ...
+
+❌hozirgi kunda vardan foydalanish tavsita etilmaydi!
+*/
