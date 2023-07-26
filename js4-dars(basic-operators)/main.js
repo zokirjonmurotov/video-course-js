9d7721c3a62d026cbbf35631cf3d5dd93383f53e
zokirjonmurotov <zokirmurotov863@gmail.com>
2023-07-26 16:29:42 +0500

    basic operators(math operators)

---
+++ js4-dars(basic-operators)/main.js
@@ -0,0 +1,50 @@
+// matematik operatorlar
+
+let a = 10;
+
+/*
+
+💡JavaScriptdagi arifemetik operatorlar:
+
+🟢qo'shish:operandlarni qo'shish uchun ishlatiladi
+
+🟢ayirish:operandlarni ayirish uchun ishlatiladi
+
+🟢ko'paytirish:operandlarni ko'paytirish uchun ishlatiladi
+
+🟢bo'lish:operandlarni bo'lish uchun ishlatiladi
+
+🟢qoldiqli bo'lish:operandlarni bo'linmasidan hosil bo'lgan qoldiqni olish uchun ishlatiladi
+
+🟢increment:operandni qiymatini 1ga oshirish uchun ishlatiladi
+
+🟢decrement:operandni qiymatini 1ga kamaytirish uchun ishlatiladi
+
+
+*/
+
+/*
+💡qo'shish(+), ayirish(-),ko'paytirish(*), bo'lish(/):
+
+console.log(4 + 2);
+*/
+
+/*
+💡qoldiqli bo'lish(%):
+console.log(17 % 3); // 5 * 3 + 2 = 17
+*/
+
+/*
+increment(post-increment, pre-increment), 
+decrement(post-decrement, pre-decrement)
+
+*/
+// console.log(a++);
+// console.log(a);
+// console.log(++a);
+// console.log(a);
+
+// console.log(a--);
+// console.log(a);
+// console.log(--a);
+// console.log(a);
