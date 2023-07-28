637ccfb9b16a0bcfbb4154bb76db745dd8c4204f
zokirjonmurotov <zokirmurotov863@gmail.com>
2023-07-28 22:51:13 +0500

    comperison operator

---
+++ solishtirish(comparison)/main.js
@@ -0,0 +1,59 @@
+// solishitirish operatorlar
+
+// 💡JavaScriptdagi solishtirish operatorlar:
+
+/*
+🔵 == : 2ta tenglik operatori operandlarning turlarini hisobga olmagan holda qiymatlarini solishtiradi va agar qiymatlar teng bo'lsa true bo'lmasa false qiymat qaytaradi
+
+*/
+
+// console.log("345" !== "345");
+
+/*
+--------------------------
+
+🔵 === : 3ta tenglik operatori operandlarning turlarini hisobga olgan holda qiymatlarini solishtiradi va agar qiymatlar teng bo'lsa true bo'lmasa false qiymat qaytaradi
+
+*/
+
+/*
+--------------------------
+
+🔵 != : undov tenglik operatori operandlarning turlarini hisobga olmagan holda qiymatlarini teng emasligini tekshiradi va agar teng bo'lmasa true teng bo'lsa false qiymat qaytaradi.
+
+*/
+
+/*
+--------------------------
+
+🔵 !== : undov 2ta tenglik operatori operandlarning turlarini hisobga olgan holda qiymatlarini teng emasligini tekshiradi va agar teng bo'lmasa true teng bo'lsa false qiymat qaytaradi.
+
+*/
+
+/*
+--------------------------
+
+🔵 > : chapdagi operand o'ngdagidan katta ekanligini tekshiradi
+
+--------------------------
+
+🔵 < : chapdagi operand o'ngdagidan kichik ekanligini tekshiradi
+
+--------------------------
+
+*/
+
+console.log(12 <= 1);
+
+/*
+
+🔵 >= : chapdagi operand o'ngdagidan katta yoki teng ekanligini tekshiradi
+
+
+--------------------------
+
+🔵 <= : chapdagi operand o'ngdagidan kichik yoki teng ekanligini tekshiradi
+
+--------------------------
+
+*/
